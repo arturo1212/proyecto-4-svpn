ee5a0a4bfb0744befa2f186af0544903c407a476
Arturo Toro <arturoantoniotr@gmail.com>
2018-04-08 19:22:35 -0400

    primer commit

---
+++ generator.py
@@ -0,0 +1,66 @@
+# Script que permite crear casos de prueba para los tableros.
+import random
+from os import listdir
+from os.path import isfile, join
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+import scipy.misc
+from scipy.misc import imshow
+import pickle
+
+def picture_russel_maze(maze, filename):
+	# Filename tiene el nombre del archivo de salida
+	#f = open("Results/SAToutput/" + filename + '.txt', 'r')	# Abrir archivo
+	plt.imsave("Results/Images/PDF/image_" + filename + ".pdf", (1-np.array(data)).reshape(maze.height, maze.width), cmap=cm.gray)
+	f.close()
+
+class russel_maze_inst():
+	def __init__(self, n, m, obstacleN = 2):
+		self.vector = [0] * n*m
+		self.obstacles = []
+		self.goal_plus  = 0
+		self.goal_minus = 0
+		self.width  = n 
+		self.height = m
+
+		# Crear meta positiva
+		self.obstacles = random.sample(range(0, n*m -1), obstacleN + 2)	
+		self.goal_plus = random.choice (self.obstacles)
+
+		# Crear meta negativa
+		self.obstacles.remove(self.goal_plus)
+		self.goal_minus = random.choice (self.obstacles)
+		self.obstacles.remove(self.goal_minus)
+
+		for pos in self.obstacles:
+			self.vector[pos] = 10
+
+		self.vector[self.goal_plus] = 1
+		self.vector[self.goal_minus] = 5
+
+def create_cases(width, height, obstacles, caseN):
+	aux = 0
+	cases = []
+	while(aux < caseN):
+		data = russel_maze_inst(width, height, obstacles)
+		if(not data in cases ):
+			#print(data)
+			cases.append(data)
+			aux+=1
+	return cases
+
+def create_test_file(width, height, cases):
+	result = str(width) + " " + str(height) + "\n"
+	for case in cases:
+		result+= ' '.join(str(e) for e in case.vector) + "\n"
+		
+	#print(result)
+	file_name = "test/Russel-maze/" + str(width) + "x" + str(height) + "_maze.cases"
+	f = open(file_name, 'w')
+	f.write(result)
+
+#width, height = 4, 3
+#cases = create_cases(width, height, 2, 20)
+#create_test_file(width, height, cases)
+#imshow((1-np.array(data.vector)).reshape(data.height, data.width))
---
+++ translator.py
@@ -0,0 +1,96 @@
+from os import listdir
+from os.path import isfile, join
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+import scipy.misc
+import generator
+
+def directory_files(mypath):
+	onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+	filenames = [(f.split("."))[0] for f in onlyfiles]
+	return filenames
+
+class maze_case():
+	def __init__(self, data, width, height):
+		self.width, self.height = width, height
+		self.matrix = (np.array(data)).reshape(height, width)
+
+def create_file_cases(filename):
+	result = []
+	f = open(filename, 'r')
+	width, height = -1, -1
+	for line in f:
+		if(width == -1):
+			[width,height] = line.split(' ')
+		else:
+			data = (line.strip()).split(' ')
+			data = list(map(int, data))
+			result.append(maze_case(data, int(width), int(height)))
+	return result
+
+def is_obstacle(pos):
+	return pos == 10
+
+def is_good(pos):
+	return pos == 1
+
+def is_bad(pos):
+	return pos == 5
+
+def solve_case(case):
+	observs = []
+	rewards = []
+	transitions = {"n":[], "s":[], "w":[], "e":[]}
+	# ------------------- Crear transiciones ------------------------- #
+	# 						 (n, s, e, w)	
+	for key, matrix in transitions: #
+		aux_matrix = case.matrix.copy()
+		if(key == "n"): # Arriba 0.8, A los lados  0.10
+
+		if(key == "s"): # Abajo 0.8 , A los lados  0.10
+		if(key == "w"): # Izqui 0.8 , Arriba/Abajo 0.10
+
+		if(key == "e"): # Derec 0.8 , Arriba/Abajo 0.10
+
+	# ------------------- Crear observaciones ------------------------ #
+	#            (left, right, neither, both, good, bad)			   #
+	for row in case.matrix:
+		for i in range(0, len(row)):
+			if(row[i] == 10):	# No crear para los obstaculos.
+				continue
+			# CASOS	
+			if(is_good(row[i])):
+				observs.append("0.0 0.0 0.0 0.0 1.0 0.0")		# Estados finales
+			elif(is_bad(row[i])):						
+				observs.append("0.0 0.0 0.0 0.0 0.0 1.0")
+			elif(i-1 < 0 or is_obstacle(row[i-1]) and (i+1 >= case.width or is_obstacle(row[i+1]))):
+				observs.append("0.0 0.0 0.0 1.0 0.0 0.0")# Pared a ambos lados
+			elif(i-1 < 0 or is_obstacle(row[i-1])):				# Caso ver a la izquierda
+				observs.append("1.0 0.0 0.0 0.0 0.0 0.0")	
+			elif(i+1 >= case.width or is_obstacle(row[i+1])):	# Caso pared a la derecha
+				observs.append("0.0 1.0 0.0 0.0 0.0 0.0")
+			else:											# No ver paredes
+				observs.append("0.0 0.0 1.0 0.0 0.0 0.0")
+
+	# ------------------ Crear costos/recompensas -------------------- #
+	for row in case.matrix:
+		for value in row:
+			if(value == 1):
+				rewards.append(1)
+			elif(value == 5):
+				rewards.append(-1)
+			elif(value == 0):
+				rewards.append(-0.04)
+	return rewards, observs
+
+
+test_files = directory_files("test/Russel-maze") # Extraer los archivos del directorio test
+file_cases = create_file_cases("test/Russel-maze/" + test_files[0] + ".cases")
+# FALTA: CREAR ARCHIVO POMDP Y Guardar .pomdp en directorio POMDP
+for case in file_cases:
+	rewards, observs = solve_case(case)	# EN PROCESO: TRANSICIONES
+	print("#############")
+	print(case.matrix)
+	print(observs)
+print(len(file_cases))
